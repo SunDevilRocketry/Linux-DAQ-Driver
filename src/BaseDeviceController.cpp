492ec8f1c380beca0a60b929fdcb5ae1f9644a1b
Thomas Jennewein <tjennewe@asu.edu>
2025-09-26 12:21:00 -0700

    feat: base abstract controller class

---
+++ src/BaseDeviceController.cpp
@@ -0,0 +1,12 @@
+#include <BaseDeviceController.hpp>
+
+class BaseDeviceController {
+    public:
+        float virtual get_voltage() {}
+        float virtual get_current() {}
+        float virtual get_pressure() {}
+        float virtual get_flow_pressure() {}
+
+        void virtual command_poll_loop() {}
+        void virtual command_execute(uint8_t command) {}
+};
