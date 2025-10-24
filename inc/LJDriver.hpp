a1b89276093a8fc6eaccb09988472fe1521d8397
niekky <trungkiencfc@gmail.com>
2025-10-24 10:59:46 -0700

    feat: added list_devices, device struct for tracking state, check connection

--- inc/LJDriver.hpp
+++ inc/LJDriver.hpp
@@ -1,15 +1,35 @@
 // #include <BaseDeviceController.hpp>
 #include <stdint.h>
 #include <LabJackM.h>
+#include <vector>
+#include <string>
+
+typedef enum LJStatus {
+    OK = 0,
+    FAILED
+} LJStatus;
+
+typedef struct LJInfo {
+    int handle;
+    int DeviceType;
+    int ConnectionType;
+    int SerialNum;
+    int Ip;
+} LJInfo;
 
 class LJDriver {
     private:
-        int handle;
+        LJInfo* driver;
 
     public:
         LJDriver();
-        void connect();
-        void close();
-        void read_address(uint32_t address, uint32_t data, void* output);
-        void write_address(uint32_t address, uint32_t data);
+        ~LJDriver();
+        LJStatus list_devices(std::vector<LJInfo> &output);
+        LJStatus connect(const char* id);
+        LJStatus close();
+        LJStatus read_address(uint32_t address, uint32_t data, void* output);
+        LJStatus write_address(uint32_t address, uint32_t data);
+
+    private:
+        bool is_connected();
 };
--- src/LJDriver.cpp
+++ src/LJDriver.cpp
@@ -1,25 +1,82 @@
 #include <LJDriver.hpp>
 
 LJDriver::LJDriver(){
-    handle = -1;
+    driver = nullptr;
 }
 
-void LJDriver::connect(){
-    int err = LJM_Open(LJM_dtANY, LJM_ctANY, "LJM_idANY", &handle);
-    // if err != 0 => Return FAILURE status (enum)
+LJDriver::~LJDriver(){
+    if (driver == nullptr) return;
+    close();
+    driver == nullptr;
+    free(driver);
 }
 
-void LJDriver::close(){
-    int err = LJM_Close(handle);
-    // if err != 0 => Return FAILURE status (enum)
+LJStatus LJDriver::connect(const char* id){
+    int handle, deviceType, connectionType, serialNum, ipAddr, port = 0;
+    int connect_status = LJM_Open(LJM_dtT7, LJM_ctANY, id, &handle);
+    int info_status = LJM_GetHandleInfo(handle, &deviceType, &connectionType, &serialNum, &ipAddr, &port, nullptr);
+    if (connect_status + info_status != OK){
+        return FAILED;
+    }
+    driver->handle = handle;
+    driver->ConnectionType = connectionType;
+    driver->DeviceType = deviceType;
+    driver->SerialNum = serialNum;
+    return OK;
 }
 
-void LJDriver::read_address(uint32_t address, uint32_t data, void* output){
-    int err = LJM_eReadAddress(handle, address, LJM_FLOAT32, (double*) output);
-    // if err != 0 => Return FAILURE status (enum)
+LJStatus LJDriver::list_devices(std::vector<LJInfo> &output){
+    const int MAX_SIZE = 20;
+    int numFound;
+    int deviceTypes[MAX_SIZE];
+    int connectionTypes[MAX_SIZE];
+    int serialNum[MAX_SIZE];
+    int ipAddr[MAX_SIZE];
+
+    int err = LJM_ListAll(LJM_dtT7, LJM_ctANY, &numFound, deviceTypes, connectionTypes, serialNum, ipAddr);
+
+    for (int i = 0; i < numFound; i++){
+        output.at(i) = {
+            0,
+            connectionTypes[i],
+            deviceTypes[i],
+            serialNum[i],
+            ipAddr[i]
+        };
+    };
+    if (err == 0){
+        return OK;
+    } else return FAILED;
 }
 
-void LJDriver::write_address(uint32_t address, uint32_t data){
-    int err = LJM_eWriteAddress(handle, address, LJM_FLOAT32, (double) data);
-    // if err != 0 => Return FAILURE status (enum)
+
+LJStatus LJDriver::close(){
+    if (!is_connected()) return FAILED;
+    
+    int err = LJM_Close(driver->handle);
+    if (err == 0){
+        return OK;
+    } else return FAILED;
+}
+
+LJStatus LJDriver::read_address(uint32_t address, uint32_t data, void* output){
+    if (!is_connected()) return FAILED;
+    
+    int err = LJM_eReadAddress(driver->handle, address, LJM_FLOAT32, (double*) output);
+    if (err == 0){
+        return OK;
+    } else return FAILED;
+}
+
+LJStatus LJDriver::write_address(uint32_t address, uint32_t data){
+    if (!is_connected()) return FAILED;
+
+    int err = LJM_eWriteAddress(driver->handle, address, LJM_FLOAT32, (double) data);
+    if (err == 0){
+        return OK;
+    } else return FAILED;
+}
+
+bool LJDriver::is_connected(){
+    return !(driver == nullptr);
 }
